1550b3e44b08398a8f0a96bda60c15f16f40fca5
Guilherme Reis <guilhermefelipedosreis@hotmail.com>
2023-01-20 14:51:28 -0500

    header and footer done and working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/main/about/about.component';
+import { ProjectsComponent } from './components/main/projects/projects.component';
+import { ResumeComponent } from './components/main/resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '',
+  redirectTo: 'about',
+  pathMatch: 'full'
+  },
+  {
+  path: 'about',
+  component: AboutComponent
+  },
+  {
+  path: 'projects',
+  component: ProjectsComponent
+  },
+  {
+  path: 'resume',
+  component: ResumeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
